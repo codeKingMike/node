603d05a13f91443e8c105d297d31deed87bbf73e
codekingmike <codekingmike@gmail.com>
2019-10-06 17:40:00 -0700

    node is FUN!

--- demo_http_url.js
+++ demo_http_url.js
@@ -4,4 +4,5 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(req.url);
     res.end();
+    console.log(req.url);
 }).listen(8080);
--- demo_querystring.js
+++ demo_querystring.js
@@ -4,6 +4,6 @@ var url = require('url');
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
+    var txt = "<ol>" + "<li>" + q.year + "</li>" + " " + "<li>" + q.month + "</li>" + "</ol>" + " " + "<h1 stlye='color:red;'>" + q.day + "</h1>";
     res.end(txt);
 }).listen(8080);
